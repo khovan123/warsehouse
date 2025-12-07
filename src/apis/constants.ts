f67b63d00028087dbd9d48e5cdedb79f494d4135
khovan <minhpnq1807@gmail.com>
2025-12-07 15:54:17 +0700

    feat: using restful-api for login

--- src/apis/constants.ts
+++ src/apis/constants.ts
@@ -1,1 +1,2 @@
 export const GRAPHQL = '/graphql/v1';
+export const LOGIN_PATH = '/auth/login';
--- src/components/organisms/AppSideBar/AppSideBar.tsx
+++ src/components/organisms/AppSideBar/AppSideBar.tsx
@@ -28,6 +28,7 @@ import type { SideBarProps } from './type';
 const AppSideBar = ({ activePath, sections, iconSize = 4, ...props }: SideBarProps) => {
   const { setBreadCrumb } = usePageContext();
   const user = useUserSelector();
+  console.log(user);
   return (
     <Sidebar {...props}>
       <SidebarHeader className="p-0">
@@ -91,8 +92,8 @@ const AppSideBar = ({ activePath, sections, iconSize = 4, ...props }: SideBarPro
           <SidebarMenuItem>
             <UserDropdown
               user={{
-                name: user!.username,
-                email: user!.email,
+                name: user?.username || 'Username',
+                email: user?.email || 'username@email.io',
                 avatar: 'https://github.com/shadcn.png',
               }}
               inSidebar={true}
