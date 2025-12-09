0078df90dc19fbbbf699fea00a9016c6b1e9961f
khovan <minhpnq1807@gmail.com>
2025-12-09 22:49:10 +0700

    fix: systax constructor

--- src/WarehouseBusiness/Application/Services/SetupsService.cs
+++ src/WarehouseBusiness/Application/Services/SetupsService.cs
@@ -3,7 +3,6 @@ using Application.Interfaces;
 using Contract.Responses;
 using Domain.Repositories;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
 
 namespace Application.Services
 {
@@ -14,7 +13,7 @@ namespace Application.Services
 
         public SetupsService(
             IWarehouseRepository warehouseRepository,
-            IBinRepository binRepository,
+            IBinRepository binRepository)
         {
             _warehouseRepository = warehouseRepository;
             _binRepository = binRepository;
