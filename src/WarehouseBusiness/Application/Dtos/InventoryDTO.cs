df2bd39063058a3261ecb9f7199286123c40a6d9
khovan <minhpnq1807@gmail.com>
2025-12-13 19:42:32 +0700

    feat: get inventories

---
+++ src/WarehouseBusiness/Application/Dtos/InventoryDTO.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Dtos
+{
+    public record InventoryDTO
+    {
+        public record Response(List<Inventory>? Inventories = default!)
+        {
+            public List<Inventory>? Inventories { get; set; } = Inventories;
+        }
+    }
+}
---
+++ src/WarehouseBusiness/Infrastructure/Repositories/InventoryRepository.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Infrastructure.DB;
+using MongoDB.Driver;
+
+namespace Infrastructure.Repositories
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private readonly IMongoCollection<Inventory> _inventory;
+
+        public InventoryRepository(MongoDbContext context)
+        {
+            _inventory = context.Inventories;
+        }
+        public async Task<List<Inventory>> GetAll(CancellationToken ct)
+        {
+            var filter = Builders<Inventory>.Filter.Empty;
+            return await _inventory.Find(filter).ToListAsync(ct);           
+        }
+
+        public async Task<Inventory> GetById(string id, CancellationToken ct)
+        {
+            var filter = Builders<Inventory>.Filter.Eq(i=>i.Id, id);
+            return await _inventory.Find(filter).FirstOrDefaultAsync(ct);
+        }
+    }
+}
