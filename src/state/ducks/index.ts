9de429df08a4d6a626ac83847c2e9d9ac7a75c0c
Anh Tú <anhtunguyen643@gmail.com>
2025-12-16 06:34:20 +0000

    feat: get stock report

--- src/state/ducks/index.ts
+++ src/state/ducks/index.ts
@@ -4,8 +4,16 @@ import { authSaga } from './auth/auth.saga';
 import { inventorySaga } from './inventory/inventory.saga';
 import { productSaga } from './product/product.saga';
 import { setupsWarehouseSaga } from './setups-warehouse/setups.saga';
+import { stockSaga } from './stock/stock.saga';
 import { toastSaga } from './toast/toast.saga';
 
 export default function* rootSaga() {
-  yield all([authSaga(), toastSaga(), productSaga(), setupsWarehouseSaga(), inventorySaga()]);
+  yield all([
+    authSaga(),
+    toastSaga(),
+    productSaga(),
+    stockSaga(),
+    setupsWarehouseSaga(),
+    inventorySaga(),
+  ]);
 }
