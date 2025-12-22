ae20c5a11179bf1be2aebcd0b0c492b41224aff8
tudeptrai <anhtunguyen643@gmail.com>
2025-12-22 20:56:54 +0700

    fix: fix prettier formatting

--- src/@types/pareto.ts
+++ src/@types/pareto.ts
@@ -1,6 +1,6 @@
 import type { Pareto, ParetoClassification } from '@/state/ducks/pareto/type';
 
 export type ParetoResponse = {
-    paretos: Pareto[];
-    classifications: ParetoClassification[];
+  paretos: Pareto[];
+  classifications: ParetoClassification[];
 };
