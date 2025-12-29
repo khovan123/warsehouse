ceec3d4efc47b1d058a0e49ad0f3eea6d094398e
khovan <minhpnq1807@gmail.com>
2025-12-29 17:27:06 +0700

    style: make constants variable in authcontroller

--- src/WarehouseBusiness/API/Common/Constants.cs
+++ src/WarehouseBusiness/API/Common/Constants.cs
@@ -2,6 +2,8 @@
 {
     public static class Constants
     {
+        public const string REFRESH_TOKEN_HEADER = "refresh_token";
+        public const string AUTH_PATH = "/api/v1/auth";
         public const string REGEX_SEARCH_ENDPOINT = @"/otp(/|$)";
         public const string REGEX_OTP_ENDPOINT = @"/search(/|$)";
     }
