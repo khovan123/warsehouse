23a38881f25c80fd9586bb0ab797ffcbf4492c84
tudeptrai <anhtunguyen643@gmail.com>
2026-01-08 08:26:10 +0700

    feat(OB-13): get material tracsaction api

--- src/WarehouseBusiness/Infrastructure/Repositories/StockRepository.cs
+++ src/WarehouseBusiness/Infrastructure/Repositories/StockRepository.cs
@@ -47,6 +47,35 @@ namespace Infrastructure.Repositories
             return await pipeline.ToListAsync(ct);
         }
 
+        public async Task<List<StockDetails>?> GetAllWithDetailsAsync(string? warehouseId, string? categoryId, CancellationToken ct)
+        {
+            var pipeline = new MongoAggregationPipeline<Stock>(_stocks)
+                .Match(s => true)
+                .Lookup(MongoCollections.Products, "productId", $"tmp_{MongoCollections.Products}")
+                .Lookup(MongoCollections.Warehouses, "warehouseId", $"tmp_{MongoCollections.Warehouses}")
+                .Lookup(MongoCollections.Bins, "binId", $"tmp_{MongoCollections.Bins}")
+                .Lookup(MongoCollections.Categories, "categoryId", $"tmp_{MongoCollections.Categories}")
+                .Project(new BsonDocument
+                {
+                    { "productId", 1 },
+                    { "warehouseId", 1 },
+                    { "binId", 1 },
+                    { "categoryId", 1 },
+                    { "onHand", 1 },
+                    { "reserved", 1 },
+                    { "available", 1 },
+                    { "averageCost", 1 },
+                    { "inventoryValue", 1 },
+                    { "product", BsonDocumentExpression.ArrayElemAt($"tmp_{MongoCollections.Products}", 0) },
+                    { "warehouse",BsonDocumentExpression.ArrayElemAt($"tmp_{MongoCollections.Warehouses}", 0) },
+                    { "bin", BsonDocumentExpression.ArrayElemAt($"tmp_{MongoCollections.Bins}", 0) },
+                    { "category", BsonDocumentExpression.ArrayElemAt($"tmp_{MongoCollections.Categories}", 0) }
+                })
+                .As<StockDetails>();
+
+            return await pipeline.ToListAsync(ct);
+        }
+
         public async Task<StockDetails?> GetByIdAsync(string id, CancellationToken ct)
         {
             var pipeline = new MongoAggregationPipeline<Stock>(_stocks)
